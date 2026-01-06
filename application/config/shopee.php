f604052dfa1c1aae994cfa3b70eab1afe0d44ef7
Alfredo Adi <alfreditdgi@gmail.com>
2026-01-06 23:52:47 +0700

    test Shopee Api #5

--- application/config/shopee.php
+++ application/config/shopee.php
@@ -1,4 +1,4 @@
 <?php
 // application/config/shopee.php
+$config['shopee_test_host'] = 'https://openplatform.sandbox.test-stable.shopee.sg';
 $config['shopee_live_host'] = 'https://partner.shopeemobile.com';
-$config['shopee_test_host'] = 'https://partner.test-stable.shopeemobile.com';
